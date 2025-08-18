d7eff16c633712735c2b8ffd4e845cc363b3eafa
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2025-08-18 01:20:57 -0300

    cambios

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,10 +1,21 @@
 import { useNavigate, Link } from "react-router-dom";
+import flyerChurrero from './juego/imagenes/flyerchurrero.png'
 const Inicio = () => {
     return (
-        <div className="w-screen h-screen flex items-center justify-center bg-gray-800">
+        <div className="w-screen h-screen flex  bg-black flex-col">
+            <div className="h-[100px] w-full bg-red-300">
+                <div className="bg-blue-600 w-[50%] h-full">
+                    kscaritas logo
+                </div>
+            </div>
+            <div className="">
+                <p className="text-white text-center font-bold">Titulo Principal</p>
+                <p className="text-white text-center">Esto es una descripcion de los juegos</p>
+            </div>
             <Link to="/churrero">
-                <div className="border-[5px] border-purple-400 p-4">
-                    <h1 className="text-white text-4xl">Juego del churrero</h1>
+                <div className="border-[5px] border-orange-400 p-4">
+                    <img className="rounded-2xl" src={flyerChurrero} alt="" />
+                    {/* <h1 className="text-white text-4xl">Juego del churrero</h1> */}
                 </div>
             </Link>
         </div>
